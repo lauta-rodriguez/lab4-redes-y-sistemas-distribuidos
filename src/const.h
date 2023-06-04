d869af3f107bf5e9022c6abfd46442f0d29714fc
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-06-03 22:33:04 -0300

    feat: add .h file to store definitions of constants

---
+++ src/const.h
@@ -0,0 +1,9 @@
+#define KIND_INFO 42
+#define KIND_HELLO 7
+#define KIND_DATA 12
+
+#define NET_HALF_LENGTH 4
+#define DEST_NODE 5
+
+// interface used to send hello and info packets
+#define REC_LNK 0

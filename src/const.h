948f05d21f6bce26290267ac45685d88fd12b4d1
lara.kurtz <lara.kurtz@mi.unc.edu.ar>
2023-06-17 19:00:48 -0300

    refactor: remove unused constants

--- src/const.h
+++ src/const.h
@@ -2,8 +2,7 @@
 #define KIND_HELLO 7
 #define KIND_DATA 12
 
-#define NET_HALF_LENGTH 4
 #define DEST_NODE 5
 
-// interface used to send hello and info packets
+// interface used to send hello packets
 #define REC_LNK 0
